17f0da5eca502e1ac430ec496c7e9e49a75fd38b
LunarCS <111065230+LunarCS@users.noreply.github.com>
2023-02-21 08:38:40 +0200

    Added 2 projectile shooting enemy, one of them can ricochet

--- Assets/Enemy/ProjectileEnemies.cs
+++ Assets/Enemy/ProjectileEnemies.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class ProjectileEnemies : Enemy
 {
-    private bool isRicochet;
+    public bool canRicochet;
+    public int maxRicochets;
+    public Collider2D enemyCollider;
+
 
     private void Start()
     {
         InvokeRepeating(nameof(ShootProjectile), 1f, abilityRate);
+        
     }
 
     void ShootProjectile()
     {
-        GameObject projectile = Instantiate(abilityPrefab, transform.position, Quaternion.identity, transform);
-        projectile.GetComponent<Projectile>().velocity *= (int) transform.localScale.x;
-        Destroy(projectile, 1f);
+        GameObject projectile = Instantiate(abilityPrefab, transform.position, Quaternion.identity);
+        Projectile projectileClass = projectile.GetComponent<Projectile>();
+        Collider2D projectileCollider = projectileClass.selfCollider;
+        Physics2D.IgnoreCollision(projectileCollider, enemyCollider);
+        projectileClass.Ricochet(canRicochet, maxRicochets);
+        projectileClass.velocity *= (int)transform.localScale.x;
     }
 }
--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -7,10 +7,34 @@ public class Projectile : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] Rigidbody2D rb;
+    public Collider2D selfCollider;
     public int velocity;
+    private int bounces;
 
     private void Start()
     {
         rb.AddForce(transform.right * velocity, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Impassable"))
+        {
+            bounces--;
+            if (bounces <= 0)
+                Destroy(gameObject);
+
+        }
+    }
+
+    public void Ricochet(bool canRicochet, int maxBounces)
+    {
+        if (canRicochet)
+            bounces = maxBounces;
+        else
+        {
+            bounces = 0;
+        }
+
     }
 }
